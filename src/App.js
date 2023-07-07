c95c150ac0954277636d3d7b5b9e3782e9285af5
haseeb_invader <rhaseeb741@gmail.com>
2023-07-07 16:56:52 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
 
 import React from 'react';
-
 // import  { useState } from 'react';
 import './App.css';
 import { UserContext } from '../src/UserContext'
 import { Routes, Route} from 'react-router-dom';
 import Index from './components/Index.jsx';
 import RequestQuote from './components/REquestQutoe/RequestQuote';
-import ProductComponent from './components/ProductOverview/ProductComponent'
+
 import { useState } from 'react'
 import CartPopup from './components/CartPopup/CartPopup';
-import { useEffect } from 'react';
 // import CheckPrice from './components/CheckPrice/CheckPrice';
 
 function App() {
 
   const [sideMenuBar, setsideMenuBar] = useState(false)
   const [sideMenuBarDropDown, setsideMenuBarDropDown] = useState(false)
-  const [sideMenuBarDropDownIndex, setsideMenuBarDropDownIndex] = useState(false);
-  const [isdraweropen , setisdraweropen]=useState(false);
-  const [isCartopen, setisCartopen] = useState(false)
- 
+  const [sideMenuBarDropDownIndex, setsideMenuBarDropDownIndex] = useState(false)
 
+  const [isCartopen, setisCartopen] = useState(false)
   return (
-    <UserContext.Provider value={{sideMenuBar,isdraweropen,setisdraweropen,isCartopen,setisCartopen, setsideMenuBar, sideMenuBarDropDown, setsideMenuBarDropDown, sideMenuBarDropDownIndex, setsideMenuBarDropDownIndex}} >
+    <UserContext.Provider value={{sideMenuBar,isCartopen,setisCartopen, setsideMenuBar, sideMenuBarDropDown, setsideMenuBarDropDown, sideMenuBarDropDownIndex, setsideMenuBarDropDownIndex}} >
      <Routes>
       <Route path='/' element={<Index/>}/>
       {/* <Route path='/' element={<CheckPrice/>}/> */}
